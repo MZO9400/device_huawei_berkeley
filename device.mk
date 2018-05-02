4e54f09f647e982c65696bcd1951954fb3d33321
LuK1337 <priv.luk@gmail.com>
2018-05-02 13:41:26 +0200

    berkeley: Reenable hisupl service
    * Our shim lets us pass ID check in
      hardware/libhardware/hardware.c @ load()

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshims_hisupl
+
 # VNDK
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vndk-compat/vndk-detect:system/bin/vndk-detect \
