946775d1c8ca8ff81e16c551dae234b0eaf052bd
MZO9400 <hamza.hameed00@yahoo.com>
2018-05-03 11:46:36 +0200

    Add Huawei Camera prebuilt
    Change-Id: I5c51d483540c91da14aa34270faf8b8be1e9be14

---
+++ prebuilt/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := HwCamera2
+LOCAL_MODULE_OWNER := honor
+LOCAL_SRC_FILES := system/priv-app/HwCamera2/HwCamera2.apk
+LOCAL_OVERRIDES_PACKAGES := Camera2
+LOCAL_CERTIFICATE := shared
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
